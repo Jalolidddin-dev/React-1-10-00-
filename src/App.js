fa0bb686ebc3a30e58958804090debbda250cff1
Dew0277 <jaloliddina98@gmail.com>
2024-10-07 09:34:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import { MdOutlineWbSunny } from "react-icons/md";
+import { IoMoon } from "react-icons/io5";
 
 function App() {
-  return <div className="group"></div>;
+  const [value, setValue] = useState(false);
+
+  const iconSet = () => {
+    if (value === true) {
+      setValue(false);
+    } else {
+      setValue(true);
+    }
+  };
+
+  console.log(value);
+
+  return (
+    <div>
+      {/* <h1>{value}</h1>
+      <button onClick={() => setValue("Salom dunyo")}>change</button> */}
+      <button onClick={iconSet}>
+        {value ? <IoMoon /> : <MdOutlineWbSunny />}
+      </button>
+    </div>
+  );
 }
 
 export default App;
